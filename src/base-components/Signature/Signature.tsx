ce575392ca07eea79bf13957679e6cd7c5645b70
Bernardo Marinho <bgmarinho96@gmail.com>
2023-09-16 00:38:30 -0300

    Making some other fixes and trying to run the project

--- src/base-components/Signature/Signature.tsx
+++ src/base-components/Signature/Signature.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import type { ChildrenType } from '@/types/childrenType';
+import type { ChildrenType } from '../../types/ChildrenType';
 import * as S from './styles';
 
 export default function Signature({ children }: ChildrenType) {
+  console.log('children >>', typeof children)
   return (
     <S.SignatureContainer>
       <S.Signature>{children}</S.Signature>
